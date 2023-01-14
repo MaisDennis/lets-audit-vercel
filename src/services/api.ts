c575f2ca56e6e51525087904812b8a9f7be8487d
MaisDennis <den.roadkill333@gmail.com>
2023-01-14 11:16:59 -0300

    added nginx and ssl to server

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://3.132.7.16:3333',
   // baseURL: 'http://localhost:3002',
+  // baseURL: 'http://3.132.7.16:3333',
+  baseURL: 'https://nodejs.vamosauditaropresidente.com',
 })
 
 export default api;
