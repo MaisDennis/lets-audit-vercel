84acc4355a4cf9dcb1b82803854507d8a3d2c7d2
MaisDennis <den.roadkill333@gmail.com>
2023-01-20 11:27:26 -0300

    added Facebook Auth to Prod 02

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,15 +1,15 @@
 import NextAuth from "next-auth"
 import FacebookProvider from "next-auth/providers/facebook";
-// import GithubProvider from "next-auth/providers/github"
+import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
-    // GithubProvider({
-    //   clientId: process.env.GITHUB_ID,
-    //   clientSecret: process.env.GITHUB_SECRET,
-    // }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
     // ...add more providers here
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
