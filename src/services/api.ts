d01c8ffad64aad2adaf5e5a0268d15d86951b0eb
MaisDennis <den.roadkill333@gmail.com>
2023-01-22 00:11:33 -0300

    added base_URL for images 02

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3002',
+  // baseURL: 'http://localhost:3002',
   // baseURL: 'http://3.132.7.16:3333',
-  // baseURL: 'https://nodejs.vamosauditaropresidente.com',
+  baseURL: 'https://nodejs.vamosauditaropresidente.com',
 })
 
 export default api;
